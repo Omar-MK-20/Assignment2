ccb02ffd37f721f5b6d8efe03dfb6d0d8278d136
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-12-09 02:44:39 +0200

    solving the leetcode bonus

--- bonus.js
+++ bonus.js
@@ -5,17 +5,29 @@
  */
 var findKthPositive = function (arr, k)
 {
+    let i = 0;
+    let current = 1;
+    let missingPositiveNumbers = 0;
 
-    for (let i = 0; i < arr.length; i++)
+    while (missingPositiveNumbers < k)
     {
-        
+        if (i < arr.length && current === arr[i])
+        {
+            i++;
+        }
+        else
+        {
+            missingPositiveNumbers++;
+            if (missingPositiveNumbers === k) return current;
+        }
+        current++;
     }
-
 
 };
 
 
 
 let arr = [2, 3, 4, 7, 11];
-let k = 5
+let k = 5;
 
+console.log(findKthPositive(arr, k));
